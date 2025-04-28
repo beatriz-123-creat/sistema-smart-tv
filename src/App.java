6a6a6410a99c34357928e105b45f734e567e2a64
beatriz <almeidasilvabeatriz82@gmai.com>
2025-04-28 17:39:49 -0300

    Revisar código, encontrar o erro.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
 public class App {
+
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        SmartTv smartTv = New SmartTv();
+        
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        //System.out.println("Canal atual: " + smartTv.canal);
+        smartTv.mudarCanal(13);
+        System.out.println("Volume atual: V o lume atual: " + smartTv.volume);
+
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual? " + smartTv.canal);
+        System.out.println("Volume Atual? " + smartTv.volume);
+        
+        smartTv.ligar();
+        System.out.println("Novo status -> TV Ligada? " + smartTv.ligada);
+        
+        smartTv.desligar();
+        System.out.println("Novo status -> TV Ligada? " + smartTv.ligada);
+
     }
 }
